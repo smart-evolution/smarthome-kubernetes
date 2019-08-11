7b93da992c41c0c13724e8c14e3c8e357af9ef8f
Oskar Szura <oskar.szura@gmail.com>
2019-08-11 12:47:33 +0200

    HOME-220 - Check by registered users are disappearing (#3)

--- master-install.sh
+++ master-install.sh
@@ -3,7 +3,7 @@ kubeadm init
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 exit
 
-kubectl create deployment smarthome-server --image=oszura/smarthome-server-prod
+kubectl apply -f smarthome-deployment.yaml
 kubectl apply -f smarthome-service.yaml
 
 # kubeadm token create --print-join-command - generate joining command
