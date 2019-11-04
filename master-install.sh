429d7c4c25d0e9651c66ff5510fc4c68caf63e1b
Oskar Szura <oskar.szura@gmail.com>
2019-11-04 09:26:38 +0100

    Update master-install.sh

--- master-install.sh
+++ master-install.sh
@@ -11,3 +11,7 @@ kubectl apply -f smarthome-deployment.yaml
 kubectl apply -f smarthome-service.yaml
 
 # kubeadm token create --print-join-command - generate joining command
+
+#hack to fix https://github.com/kubernetes/kubernetes/issues/39823
+sudo iptables -A FORWARD -j ACCEPT
+
