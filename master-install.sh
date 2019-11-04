f616339d4d3f629687c50e06e82c49832e7267cf
Oskar Szura <oskar.szura@gmail.com>
2019-11-04 10:07:39 +0100

    Update master-install.sh

--- master-install.sh
+++ master-install.sh
@@ -7,7 +7,9 @@ sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
-# kubeadm token create --print-join-command - generate joining command
-
 #hack to fix https://github.com/kubernetes/kubernetes/issues/39823
 sudo iptables -A FORWARD -j ACCEPT
+
+su -
+kubeadm token create --print-join-command - generate joining command
+exit
