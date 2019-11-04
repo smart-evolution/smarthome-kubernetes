e32225948e098000567edcb3256a369accddaef1
Oskar Szura <oskar.szura@gmail.com>
2019-11-04 10:10:27 +0100

    Update node-install.sh

--- node-install.sh
+++ node-install.sh
@@ -1,7 +1,7 @@
 su -
 sudo swapoff -a
 
-#generate join comman on master and paste here
+#generate join command on master and paste here
 
 #hack to fix https://github.com/kubernetes/kubernetes/issues/39823
 iptables -A FORWARD -j ACCEPT
