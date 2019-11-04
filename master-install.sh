acfecccffe5c8935304f0c4fd309d49a3abd9452
Oskar Szura <oskar.szura@gmail.com>
2019-11-04 10:05:38 +0100

    Update master-install.sh

--- master-install.sh
+++ master-install.sh
@@ -7,9 +7,6 @@ sudo chown $(id -u):$(id -g) $HOME/admin.conf
 export KUBECONFIG=$HOME/admin.conf
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
-kubectl apply -f smarthome-deployment.yaml
-kubectl apply -f smarthome-service.yaml
-
 # kubeadm token create --print-join-command - generate joining command
 
 #hack to fix https://github.com/kubernetes/kubernetes/issues/39823
