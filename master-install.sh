7d92b4ac7b145ab0f97b3dd77b4615c46dae1802
Oskar Szura <oskar.szura@gmail.com>
2019-11-05 17:10:31 +0100

    HOME-252 - Kubernetes adjustments to separated front-end / back-end pods (#5)

--- master-install.sh
+++ master-install.sh
@@ -1,9 +1,15 @@
 su -
 kubeadm init
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 exit
 
-kubectl apply -f smarthome-deployment.yaml
-kubectl apply -f smarthome-service.yaml
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
-# kubeadm token create --print-join-command - generate joining command
+#hack to fix https://github.com/kubernetes/kubernetes/issues/39823
+sudo iptables -A FORWARD -j ACCEPT
+
+su -
+kubeadm token create --print-join-command
+exit
--- node-install.sh
+++ node-install.sh
@@ -1,2 +1,7 @@
 su -
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+sudo swapoff -a
+
+#generate join command on master and paste here
+
+#hack to fix https://github.com/kubernetes/kubernetes/issues/39823
+iptables -A FORWARD -j ACCEPT
