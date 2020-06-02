0dfc88a0558b40fd1df8bfc7014db960b0b62971
Oskar Szura <oskar.szura@gmail.com>
2020-06-02 19:46:09 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,4 @@ export KUBECONFIG=$HOME/admin.conf
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 #hack to fix https://github.com/kubernetes/kubernetes/issues/39823
-iptables -A FORWARD -j ACCEPT
+sudo iptables -A FORWARD -j ACCEPT
