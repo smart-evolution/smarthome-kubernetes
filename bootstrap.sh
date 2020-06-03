b0ed204292998c698dd77ff9e0592772cef1cf59
Oskar Szura <oskar.szura@gmail.com>
2020-06-03 08:57:00 +0200

    HOME-302 - Create bootstrap script (#11)

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+systemctl start cear
+
+cp /etc/kubernetes/admin.conf $HOME/
+chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+#hack to fix https://github.com/kubernetes/kubernetes/issues/39823
+sudo iptables -A FORWARD -j ACCEPT
